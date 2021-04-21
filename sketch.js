10ef36c1c15c14d981d70647571cab490ccbd592
AryanKalra12345 <71889098+AryanKalra12345@users.noreply.github.com>
2021-04-21 23:04:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,16 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint=Matter.Constraint;
+var bg1,backgroundImg,bg2;
+var bg="bg2.jpg"
  
 var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20,box21,box22,box23,box24,box25;
 var ground,slingshot;
 var stand1,stand2;
+function preload(){
+    getTime();
+    bg=loadImage("bg2.jpg")
+}
 function setup(){
     var canvas = createCanvas(900,400);
     engine = Engine.create();
@@ -48,7 +54,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background(bg)
     Engine.update(engine);
 
     stand1.display();
@@ -85,11 +91,27 @@ function draw(){
      
    ellipseMode(RADIUS)
     ellipse(ball.position.x,ball.position.y,20,20);
-    
+       
 }
 function mouseDragged(){
     Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY})
 }
 function mouseReleased(){
     slingshot.fly();
+
+}
+async function getTime(){
+    var response  = await fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+    var responceaz = await response.json()
+    //console.log(responceaz)
+    var responseaq = responceaz.datetime
+    //console.log(responseaq)
+    var responceq = responseaq.slice(11,19)
+    //console.log(responceq)
+    if(responceq>=06&&responceq<=19){
+        bg = "bg2.jpg"
+    }else{
+        bg = "bg1.png"
+    }
+    backgroundImg = loadImage(bg);
 }
